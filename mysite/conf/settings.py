3feda80169f5542a9e5ab6335914a989da05fc9d
JACruz812 <jcruz5@umbc.edu>
2021-05-05 16:31:11 -0400

    Manual push to fix migrations and url.py

--- mysite/conf/settings.py
+++ mysite/conf/settings.py
@@ -74,7 +74,7 @@ WSGI_APPLICATION = 'conf.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3.storyboard',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
--- mysite/conf/urls.py
+++ mysite/conf/urls.py
@@ -38,6 +38,6 @@ urlpatterns = [
     path('refresh/', views.refresh_story, name="refresh_story"),
     path('display_clues/', views.display_clues, name="display_clues"),
     path('return_to_editor/', views.return_to_editor, name="return_to_editor"),
-    path('create_printable/', views.create_printable, name="create_printable")
+    path('create_printable/', views.create_printable, name="create_printable"),
     path('display_clues_on_page/', views.display_clues_on_page, name="display_clues_on_page")
 ]
