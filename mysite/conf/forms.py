5a446c80c7bac79a9f14f00d3973ddd680a56110
Jason Carey <jcarey3@umbc.edu>
2021-05-05 16:45:00 -0400

    Forms.py can now recieve the titles from views.py

--- mysite/conf/forms.py
+++ mysite/conf/forms.py
@@ -1,9 +1,9 @@
 from django import forms
 
 class LoadForm(forms.Form):
-    choices=[]
-    s_title=''
-    def __init__(self, *args, **kwargs):
-        choices=[]
-        s_title=''
-    s_title=forms.ChoiceField(choices=choices,widget=forms.RadioSelect)
+    story_title=forms.ChoiceField(label='Title',choices=[],widget=forms.RadioSelect)
+    def __init__(self, choices=[],s_title="",*args, **kwargs):
+        super().__init__(*args,**kwargs)
+        self.fields['story_title'].choices=choices
+        s_title=s_title
+    
